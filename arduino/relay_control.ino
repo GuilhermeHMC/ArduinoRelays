3185f7177cc291d1a315ea9b676e215d2561f0cc
Guilherme H. Marques Cardoso <87186496+GuilhermeHMC@users.noreply.github.com>
2025-05-03 01:04:55 -0300

    Update relay_control.ino

--- arduino/relay_control.ino
+++ arduino/relay_control.ino
@@ -1,1 +1,23 @@
+const int relayPins[] = {8, 9}; // D8 (Relé 1), D9 (Relé 2)
 
+void setup() {
+  Serial.begin(9600);
+  for (int i = 0; i < 2; i++) {
+    pinMode(relayPins[i], OUTPUT);
+    digitalWrite(relayPins[i], HIGH); // Inicia desligado
+  }
+}
+
+void loop() {
+  if (Serial.available()) {
+    String cmd = Serial.readStringUntil('\n');
+    
+    if (cmd == "R1_ON") digitalWrite(8, LOW);
+    else if (cmd == "R1_OFF") digitalWrite(8, HIGH);
+    else if (cmd == "R2_ON") digitalWrite(9, LOW);
+    else if (cmd == "R2_OFF") digitalWrite(9, HIGH);
+    
+    Serial.print("Exec:"); // Confirmação
+    Serial.println(cmd);
+  }
+}
